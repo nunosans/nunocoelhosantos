0b811fd66d38baf39b35c537c7683519e8c9da66
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-11-08 18:24:54 +0000

    Kent and Curwen Layout and Artwork

--- en/work/kent-and-curwen/index.php
+++ en/work/kent-and-curwen/index.php
@@ -15,7 +15,24 @@
 
 <div class="project" id="kent-and-curwen">
 
-  <!-- Work goes here -->
+  <img class="full" width="1264" height="902" src="/media/images/kent-and-curwen-on-an-ipad.jpg">
+
+  <img class="clear side" width="1264" height="481" src="/media/images/kent-and-curwen-logo-animation.gif">
+  <div class="txt">
+    <h2>Kent & Curwen</h2>
+    <h6>Resposive Design and Front-end Development</h6>
+    <p>
+      Lorem ipsum dolor sit amet, lobortis id nam erat, in minim vestibulum in nulla magna
+      donec. Sed per et, orci nulla platea quisque, vestibulum ut integer semper elit mi urna,
+      duis wisi cras nulla metus ea. Sem odio pellentesque wisi arcu nunc. Arcu feugiat amet
+      mattis vulputate. Non leo lacinia, est ipsum, ac eros elit, potenti donec in, cum cursus
+      placerat fringilla dapibus vel mollis. Voluptas donec proin, sed quis, sit sed faucibus
+      lectus viverra wisi. Mi a ac, sapien blandit sem tincidunt sapien justo. Nisl porttitor,
+      maecenas tortor sed, a potenti. Vitae sodales orci euismod sapien nonummy enim, varius eros ac.
+    </p>
+  </div>
+
+  <img class="site-plan" width="1166" height="4056" src="/media/images/kent-and-curwen-website-plan.jpg">
 
 </div>
 
