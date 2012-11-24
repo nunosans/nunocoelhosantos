1242974f993fb8a00210bdee6e18099e98a4e827
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-11-24 20:11:39 +0000

    Hash todo

--- en/work/hash-todo/index.php
+++ en/work/hash-todo/index.php
@@ -1,6 +1,7 @@
 <?
   $title='#todo';
-  $description='User interface and experience of the Twitter-powered to-do list app';
+  $description='User interface and user experience of the Twitter-powered to-do
+  list app for iOS and Android';
   require_once('../../header.inc.php');
 ?>
 
@@ -15,35 +16,49 @@
 
 <div class="project" id="hash-todo">
 
-  <img class="full" width="1080" height="1080" src="http://media.nunocoelhosantos.com/images/hash-todo-main.jpg">
+  <img class="full" width="1080" height="1080"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-main.jpg"
+    alt="">
 
   <div class="clear txt">
     <h2>#todo</h2>
-    <h6>UI, UX, Icon Design</h6>
+    <h6><acronym title="User Interface">UI</acronym>, <acronym title="User Experience">UX</acronym> and Icon Design</h6>
     <p>
-      Interface design for the Twitter-powered to-do list. #todo, pronounced "hash to-do",
-      is an utility app that let's you share things you always wanted to do with your
-      twitter network and keeps all your dreams, tasks or wishes in one single view.
-      This application was developed for iOS and Android.
+      Interface design for the <a href="http://twitter.com" target="_blank">
+      Twitter</a>-powered to-do list developed for <a href="http://apple.com/iphone"
+      target="_blank">iPhone</a> and <a href="http://www.android.com/"
+      target="_blank"> Android</a>. #todo, pronounced "hash to-do",
+      is an utility app that let’s you share dreams, tasks and ambitions with
+      your Twitter network automatically.
     </p>
     <p class="icon-globe-1">
       <a href="https://itunes.apple.com/us/app/todo-twitter-powered-to-do/id538969906?mt=8"
       target="_blank">#todo on the App Store</a>
     </p>
-    <!--<p class="icon-globe-1">
-      <a href=""
-      target="_blank">#todo on the Android Market</a>
-    </p>-->
   </div>
 
-  <img class="no-margin right half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-1.jpg">
-  <img class="no-margin clear half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-2.jpg">
-  <img class="no-margin half"       width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-3.jpg">
-  <img class="no-margin clear half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-4.jpg">
-  <img class="half"                 width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-5.jpg">
+  <img class="no-margin right half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-1.jpg"
+    alt="">
+  <img class="no-margin clear half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-2.jpg"
+    alt="">
+  <img class="no-margin half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-3.jpg"
+    alt="">
+  <img class="no-margin clear half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-4.jpg"
+    alt="">
+  <img class="half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-5.jpg"
+    alt="">
 
-  <img class="no-margin clear half" width="624" height="1000" src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-1.jpg">
-  <img class="no-margin half"       width="624" height="1000" src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-2.jpg">
+  <img class="no-margin clear half" width="624" height="1000"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-1.jpg"
+    alt="">
+  <img class="no-margin half" width="624" height="1000"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-2.jpg"
+    alt="">
 
 </div>
 
--- pt/trabalho/hash-todo/index.php
+++ pt/trabalho/hash-todo/index.php
@@ -15,35 +15,49 @@
 
 <div class="project" id="hash-todo">
 
-  <img class="full" width="1080" height="1080" src="http://media.nunocoelhosantos.com/images/hash-todo-main.jpg">
+  <img class="full" width="1080" height="1080"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-main.jpg"
+    alt="">
 
   <div class="clear txt">
     <h2>#todo</h2>
-    <h6>UI, UX, Icon Design</h6>
+    <h6>Design do Interface, Experiência e Ícone</h6>
     <p>
-      Interface design for the Twitter-powered to-do list. #todo, pronounced "hash to-do",
-      is an utility app that let's you share things you always wanted to do with your
-      twitter network and keeps all your dreams, tasks or wishes in one single view.
-      This application was developed for iOS and Android.
+      Design do interface para a lista de tarefas gerida pelo
+      <a href="http://twitter.com/?lang=pt" target="_blank">Twitter</a> para
+      <a href="http://apple.com/pt/iphone/" target="_blank">iPhone</a>
+      e <a href="http://www.android.com/" target="_blank">Android</a>. #todo,
+      pronunciado “Hash to do” é um utilitário que facilita
+      partilhar objectivos, sonhos e ambições no Twitter automáticamente.
     </p>
     <p class="icon-globe-1">
-      <a href="https://itunes.apple.com/us/app/todo-twitter-powered-to-do/id538969906?mt=8"
-      target="_blank">#todo on the App Store</a>
+      <a href="https://itunes.apple.com/pt/app/todo-twitter-powered-to-do/id538969906?mt=8"
+      target="_blank">#todo na App Store</a>
     </p>
-    <!--<p class="icon-globe-1">
-      <a href=""
-      target="_blank">#todo on the Android Market</a>
-    </p>-->
   </div>
 
-  <img class="no-margin right half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-1.jpg">
-  <img class="no-margin clear half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-2.jpg">
-  <img class="no-margin half"       width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-3.jpg">
-  <img class="no-margin clear half" width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-4.jpg">
-  <img class="half"                 width="624" height="870" src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-5.jpg">
+  <img class="no-margin right half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-1.jpg"
+    alt="">
+  <img class="no-margin clear half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-2.jpg"
+    alt="">
+  <img class="no-margin half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-3.jpg"
+    alt="">
+  <img class="no-margin clear half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-4.jpg"
+    alt="">
+  <img class="half" width="624" height="870"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-ios-screen-5.jpg"
+    alt="">
 
-  <img class="no-margin clear half" width="624" height="1000" src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-1.jpg">
-  <img class="no-margin half"       width="624" height="1000" src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-2.jpg">
+  <img class="no-margin clear half" width="624" height="1000"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-1.jpg"
+    alt="">
+  <img class="no-margin half" width="624" height="1000"
+    src="http://media.nunocoelhosantos.com/images/hash-todo-android-screen-2.jpg"
+    alt="">
 
 </div>
 
