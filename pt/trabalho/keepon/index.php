5ac3a4dbb4ef60c36f6c6f8d869eefe9694294ae
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2012-12-02 16:49:48 +0000

    Keepon

--- pt/trabalho/keepon/index.php
+++ pt/trabalho/keepon/index.php
@@ -22,17 +22,36 @@
     <p>
       This iPhone application is a client for the Mac application
       <a href="http://v1.kickoffapp.com/" target="_blank">Kickoff</a>. Kickoff
-      is a project manager tool with shared notes, chat, tasks and files. Keepon,
-      this mobile client, seeks to make all information available on-the-go
+      is a project manager tool with shared notes, chat, tasks and files. Keepon
+      seeks to make all information available on-the-go
       and allows the user to stay updated with the progress of their work,
       assign new tasks and view files from the project folder. We wanted to
       keep the transition between the desktop and the mobile app simple and
       intuitive, sharing a similar structure and hierarchy but using the native
       components and established navigation systems.
     </p>
+    <p>
+      Esta é uma aplicação para iPhone que acompanha a aplicação
+      <a href="http://v1.kickoffapp.com/" target="_blank">Kickoff</a> para
+      Mac OSX. Kickoff é um gestor de projectos onde equipas podem partilhar
+      notas, tarefas, ficheiros e conversar. O Keepon procura transportar
+      toda essa informação no ecrã do telemóvel. O meu objectivo foi tornar
+      a transição do computador para o telemóvel muito intuitiva utilizando
+      uma logica e hierarquia semelhante, mas tomando partido das tipologias
+      de navegação do iOS.
+    </p>
+    <p>
+      The current version, 1.1, has improved usability on the original version
+      and fewer bugs. The next version of the app, 1.2, will include a
+      completely bespoke <acronym title="User Interface">UI</acronym>.
+    </p>
+    <p>
+      A versão actual, 1.1, torna a utilização da aplicação mais simples e
+      reduz o numero de erros em comparação à versão original. A próxima versão
+      1.2 vai incluir um interface completamente aperfeiçoado.
     <p class="icon-globe-1">
-      <a href="https://itunes.apple.com/us/app/keepon/id533319825?ls=1&mt=8"
-      target="_blank">Keepon on the App Store</a>
+      <a href="https://itunes.apple.com/pt/app/keepon/id533319825?ls=1&mt=8"
+      target="_blank">Keepon na App Store</a>
     </p>
   </div>
   <img class="side" width="1450" height="1100"
