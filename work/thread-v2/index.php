5f4de58a4f2abdd0bf4922fc12ad734d05f9a763
Nuno Coelho Santos <nuno@nunocoelhosantos.com>
2015-07-12 16:44:45 +0100

    Dummy content on Thread V2 page

--- work/thread-v2/index.php
+++ work/thread-v2/index.php
@@ -18,6 +18,92 @@
 
 <div class="project" id="thread-v2">
 
+  <!-- Home desktop and mobile interface image -->
+  <img class="clear side" width="1008" height="669"
+    src=""
+    alt="Home desktop and mobile interface image">
+  <div class="txt">
+    <h2>Thread V2</h2>
+    <h6>Egestas, Ullamcorper amet Porta</h6>
+    <p>
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus
+      sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi
+      leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+      lacinia odio sem nec elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere
+      velit aliquet.
+    </p>
+  </div>
+
+  <!-- Browse desktop interface image -->
+  <video class="full site" width="1418" height="980" muted autoplay loop>
+    <source src="/media/videos/thread-v2-browse.mp4"  type="video/mp4">
+    <source src="/media/videos/thread-v2-browse.webm" type="video/webm">
+    <source src="/media/videos/thread-v2-browse.ogg"  type="video/ogg">
+  </video>
+
+  <div class="clear txt">
+    <p>
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus
+      sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi
+      leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+      lacinia odio sem nec elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere
+      velit aliquet.
+    </p>
+  </div>
+  <div class="txt">
+    <p>
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus
+      sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi
+      leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+      lacinia odio sem nec elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere
+      velit aliquet.
+    </p>
+  </div>
+
+  <!-- Home mobile video -->
+  <img class="clear half" width="1008" height="669"
+    src=""
+    alt="Home mobile video">
+  <!-- Browse mobile video -->
+  <img class="half" width="1008" height="669"
+    src=""
+    alt="Browse mobile video">
+
+  <!-- Add to cart animation on desktop video -->
+  <img class="full" width="1008" height="669"
+    src=""
+    alt="Add to cart animation on desktop video">
+
+  <div class="clear txt">
+    <p>
+      Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus
+      sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Morbi
+      leo risus, porta ac consectetur ac, vestibulum at eros.
+    </p>
+    <p>
+      Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+      lacinia odio sem nec elit. Lorem ipsum dolor sit amet, consectetur
+      adipiscing elit. Integer posuere erat a ante venenatis dapibus posuere
+      velit aliquet.
+    </p>
+  </div>
+  <!-- Messages on desktop -->
+  <img class="side" width="1008" height="669"
+    src=""
+    alt="Messages on desktop">
+
 </div>
 
 <?php require_once('../../_footer.php'); ?>
